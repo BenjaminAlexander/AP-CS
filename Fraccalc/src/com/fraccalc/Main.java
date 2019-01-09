8e00e8837307f2e0097b04ae8fcd531de62450fb
Benjamin Alexander <benalexander71792@gmail.com>
2019-01-08 21:15:19 -0800

    Completed CP 1
    Completed CP 1 i.e. Looper

--- Fraccalc/src/com/fraccalc/Main.java
+++ Fraccalc/src/com/fraccalc/Main.java
@@ -1,8 +1,42 @@
 package com.fraccalc;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        looper();
+    }
+
+    public static void looper() {
+        Scanner userInput = new Scanner(System.in);
+        String input;
+
+        //initial input request
+        System.out.println("Enter something: ");
+        input = userInput.nextLine();
+
+
+        while (!input.equals("quit")) {
+            System.out.println("The input is: " + input);
+
+            handleInput(input);
+
+            //start the loop again
+            System.out.println("Enter something: ");
+            input = userInput.nextLine();
+        }
+    }
+
+    public static void handleInput(String input) {
+        String[] tokens = input.split(" ");
+
+        if (tokens.length != 3) {
+            System.out.println("ERROR: the input does not contain exactly 3 tokens.");
+        } else {
+            for (String token : tokens) {
+                System.out.println(token);
+            }
+        }
     }
 }
