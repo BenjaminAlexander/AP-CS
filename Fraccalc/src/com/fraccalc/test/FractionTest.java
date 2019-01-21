b88966f75bf270e682beaa15d424de960321bc6e
Benjamin Alexander <benalexander71792@gmail.com>
2019-01-21 13:07:05 -0800

    adding junit

---
+++ Fraccalc/src/com/fraccalc/test/FractionTest.java
@@ -0,0 +1,12 @@
+package com.fraccalc.test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class FractionTest {
+    @Test
+    public void toStringTest() {
+        assertEquals(6, 6);
+    }
+
+}
